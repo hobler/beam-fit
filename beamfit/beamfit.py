Final GUI update and calculation fix
Fix a calculation error in the pearson_function_fast where pi was squared
and should have been the square root of pi

Change some GUI stuff
- Remove whitespace under the fit button
- Change the order of the File menu
- Rename options in the graph menu and made Legend on by default
- Change the layout of the "Change x values" window
- Use a spinbox for the n value
- Put the parameter notebook in a frame and made it, so it doesn't resize

Add some docstrings and update the README

beamfit/beamfit.py
@@ -70,12 +70,9 @@ def pearson_function_fast(x, beta, sigma):
     beta_san = np.where(np.equal(beta, 3), 0, beta)
     m = np.where(beta_eq_3, 0, (5 * beta_san - 9) / (6 - 2 * beta_san))
 
-    a = np.where(beta_eq_3, np.sqrt(2) * sigma,
-                 sigma * np.sqrt(np.where(
-                     beta_gt_3,
-                     np.abs(2 * m + 3),
-                     2 * m + 3))
-                 )
+    a = sigma * np.sqrt(np.where(beta_eq_3, 2, np.where(beta_gt_3,
+                                                        np.abs(2 * m + 3),
+                                                        2 * m + 3)))
 
     beta_func = np.where(beta_between_2_3, sc.beta(0.5, m + 0.5),
                          np.where(beta_gt_3, sc.beta(0.5, np.abs(m)), 0))
@@ -89,7 +86,7 @@ def pearson_function_fast(x, beta, sigma):
         f = np.where(beta_between_2_3,
                      ((m + 0.5) / (np.pi * a))
                      * beta_func * ((1 - san_fract) ** m),
-                     np.where(beta_eq_3, (1 / ((np.pi ** 2) * a))
+                     np.where(beta_eq_3, (1 / ((np.sqrt(np.pi)) * a))
                               * np.exp(-san_fract),
                               np.where(beta_gt_3,
                                        (np.abs(m + 0.5) / (np.pi * a))